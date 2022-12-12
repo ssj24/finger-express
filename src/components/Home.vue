3fdab9a86b24620e751f09e0b9a1d156702035cb
ssj24 <sujee0824@gmail.com>
2022-12-12 16:41:07 +0900

    tosspay

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -48,6 +48,7 @@
         >신청하기</v-btn>
       </v-col>
     </v-row>
+    <button @click="tosspay">toss</button>
     <!-- components -->
     <Process data-aos="fade-up" data-aos-anchor-placement="top-bottom" />
     <Price data-aos="fade-up" data-aos-anchor-placement="center-bottom" />
@@ -87,6 +88,9 @@
 import Process from './Process.vue';
 import Review from './Review.vue';
 import Price from './Price';
+
+const clientKey = 'test_ck_Z0RnYX2w532oybBdKqlVNeyqApQE'
+const tossPayments = window.TossPayments(clientKey) // 클라이언트 키로 초기화하기
 
 export default {
   name: 'LandingHome',
@@ -181,6 +185,29 @@ export default {
   methods: {
     clicked() {
       this.$emit('clicked', true);
+    },
+    tosspay() {
+
+      tossPayments.requestPayment('카드', { // 결제 수단 파라미터
+      // 결제 정보 파라미터
+      amount: 15000,
+      orderId: 'kaY-LJVfOxm1RDrbVZcqR',
+      orderName: '토스 티셔츠 외 2건',
+      customerName: '박토스',
+      successUrl: 'http://localhost:8080/success',
+      failUrl: 'http://localhost:8080/fail',
+    })
+    .then(res => alert(res))
+    .catch(function (error) {
+      if (error.code === 'USER_CANCEL') {
+        // 결제 고객이 결제창을 닫았을 때 에러 처리
+        alert('user', error);
+      } else if (error.code === 'INVALID_CARD_COMPANY') {
+        // 유효하지 않은 카드 코드에 대한 에러 처리
+        alert('card', error);
+      }
+    })
+
     }
   }
 }
